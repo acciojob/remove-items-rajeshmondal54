2400da75428ff335ed11707216206bcba2eb529f
rajeshmondal54 <rm4223536@gmail.com>
2024-07-25 01:23:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+document.querySelector('input[type="button"]').addEventListener('click', function() {
+    const colorSelect = document.getElementById('colorSelect');
+    const selectedOptionIndex = colorSelect.selectedIndex;
+    
+    if (selectedOptionIndex >= 0) {
+        colorSelect.remove(selectedOptionIndex);
+    }
+});
